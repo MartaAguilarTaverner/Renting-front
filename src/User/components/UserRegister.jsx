f67d8c8634c29b86ebc169b792da0ef42d264409
MartaAguilarTaverner <martaaguilartaverner@gmail.com>
2022-12-07 00:11:19 +0100

    User Register New and Functional. Expecting controllers and change routes and the conection with the DB

--- src/User/components/UserRegister.jsx
+++ src/User/components/UserRegister.jsx
@@ -1,102 +1,112 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { Calendar } from "primereact/calendar";
 import { Password } from "primereact/password";
 import { Checkbox } from "primereact/checkbox";
-import { Dialog } from "primereact/dialog";
-import { Divider } from "primereact/divider";
-import { CalendarDemo } from "../hooks/calendar.hooks"
+import { Dropdown } from "primereact/dropdown";
+//import { throwUnexpectedConfigError } from "@craco/craco";
 
-// const checkEmailRegexp = (email) =>
-//     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
-
-// // const checkRegisterDataIsValid = (email, password, name, dateBirth) => {
-// //     let hasErrors = false;
-
-// //     if (!email || !checkEmailRegexp || !password) {
-// //         hasErrors = true;
-// //     }
-// //     else if () {
-// //         hasErrors = true;
-// //     }
-
-// //     return hasErrors;
-// // };
-
-const UserRegister = () => {
+export const FormRegister = () => {
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [dateBirth, setDateBirth] = useState("");
-    const { onSubmitCalendar } = CalendarDemo();
-
-
-    const onSubmit = () => {};
+    const [birthDate, setBirthDate] = useState(new Date());
+    const [subscriptionOptions, setSubscriptionOptions] = useState([
+        {
+            label: "Try Free 1 month",
+            value: "try",
+        },
+        {
+            label: "6 Months",
+            value: "6m",
+        },
+        {
+            label: "1 Year",
+            value: "1y",
+        },
+    ]);
+    const [subscription, setSubscription] = useState("");
+    const [accept, setAccept] = useState();
 
     return (
-        <div className="flex justify-content-center align-items-center">
+        <div className="flex justify-content-center">
             <div className="card">
                 <h5 className="text-center">Register</h5>
-                <div className="field">
-                    <span className="p-float-label">
-                        <InputText
-                            id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <label htmlFor="name">Name</label>
-                    </span>
-                </div>
-                <div className="field">
-                    <span className="p-float-label">
-                        <InputText
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <label htmlFor="email">Email</label>
-                    </span>
-                </div>
-                <div className="field">
-                    <span className="p-float-label">
-                        <InputText
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <label htmlFor="password">Password</label>
-                    </span>
-                </div>
-                
-                {/* <div className="field">
-                    <span className="p-float-label">
-                        <InputText
-                            id="dateBirth"
-                            value={dateBirth}
-                            onChange={(e) => setDateBirth(e.target.value)}
-                        />
-                        <label htmlFor="dateBirth">Birth date</label>
-                    </span>
-                </div> */}
-
-                <div className="p-fluid grid formgrid">
-                    <div className="field col-12 md:col-4">
-                        <label htmlFor="basic">Basic</label>
-                        <Calendar onChange={(e) => onSubmitCalendar(e.value)} />
+                <form className="p-fluid">
+                    <div className="field">
+                        <span className="p-float-label">
+                            <InputText
+                                id="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <label htmlFor="name">name</label>
+                        </span>
                     </div>
-                </div>
-                <div className="field flex justify-content-center">
-                    <Button
-                        label="Submit"
-                        className="mt-2 p-button-help justify-content-center"
-                        onClick={onSubmit}
-                    />
-                </div>
+                    <div className="field">
+                        <span className="p-float-label p-input-icon-right">
+                            <i className="pi pi-envelope" />
+                            <InputText
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <label htmlFor="email">Email</label>
+                        </span>
+                    </div>
+                    <div className="field">
+                        <span className="p-float-label">
+                            <Password
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <label htmlFor="password">Password*</label>
+                        </span>
+                    </div>
+                    <div className="field">
+                        <span className="p-float-label">
+                            <Calendar
+                                id="birthDate"
+                                value={birthDate}
+                                onChange={(e) => setBirthDate(e.target.value)}
+                                dateFormat="dd/mm/yy"
+                                mask="99/99/9999"
+                                showIcon
+                            />
+                            <label htmlFor="date">Birthday</label>
+                        </span>
+                    </div>
+                    <div className="subscription">
+                        <span className="p-float-label">
+                            <Dropdown
+                                id="subscription"
+                                value={subscription}
+                                onChange={(e) => setSubscription(e.value)}
+                                options={subscriptionOptions}
+                            />
+                            <label htmlFor="subscription">
+                                Select Subscription Plan
+                            </label>
+                        </span>
+                    </div>
+                    <div className="field-checkbox">
+                        <Checkbox
+                            inputId="accept"
+                            name="accept"
+                            checked={accept}
+                            onChange={(e) => setAccept(e.checked)}
+                        />
+                        <label htmlFor="accept">
+                            I agree to the terms and conditions*
+                        </label>
+                    </div>
+                    <Button type="submit" label="Submit" className="mt-2" />
+                </form>
             </div>
         </div>
     );
 };
 
-export default UserRegister;
+export default FormRegister;
